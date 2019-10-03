240e4cc4e6d51d3531e85af6286c3b6e53d086f7
Marcos Felipe <omarkdev@gmail.com>
2019-10-02 22:52:38 -0300

    Start function settings in start theme

--- theme-btgw2019-start/functions.php
+++ theme-btgw2019-start/functions.php
@@ -1,1 +1,21 @@
 <?php
+
+/*
+ * Let WordPress manage the document title.
+ */
+add_theme_support( 'title-tag' );
+
+/*
+ * Enable support for Post Thumbnails on posts and pages.
+ */
+add_theme_support( 'post-thumbnails' );
+
+/*
+ * Switch default core markup for search form, comment form, and comments
+ * to output valid HTML5.
+ */
+add_theme_support( 'html5', array(
+    'search-form',
+    'gallery',
+    'caption',
+) );
