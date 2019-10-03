61278f635a441cb2534537e3dfc54fd81bafdd47
Marcos Felipe <omarkdev@gmail.com>
2019-10-02 22:53:59 -0300

    Enqueue bootstrap, jquery and popper style in start theme

--- theme-btgw2019-start/functions.php
+++ theme-btgw2019-start/functions.php
@@ -34,6 +34,12 @@ add_action( 'init', 'theme_nav_init' );
  * Enqueue scripts and styles.
  */
 function theme_scripts_styles() {
+    wp_enqueue_style( 'bootstrap', get_template_directory_uri() . '/assets/css/bootstrap.min.css', array(), wp_get_theme()->get( 'Version' ) );
     wp_enqueue_style( 'btgw2019-start/style', get_stylesheet_uri(), array(), wp_get_theme()->get( 'Version' ) );
+
+    wp_deregister_script('jquery');
+    wp_enqueue_script('jquery', get_template_directory_uri() . '/assets/js/jquery.min.js', array(), null, true);
+    wp_enqueue_script('bootstrap', get_template_directory_uri() . '/assets/js/bootstrap.min.js', array('jquery'), null, true);
+    wp_enqueue_script('popper', get_template_directory_uri() . '/assets/js/popper.min.js', array('jquery', 'bootstrap'), null, true);
 }
 add_action( 'wp_enqueue_scripts', 'theme_scripts_styles' );
